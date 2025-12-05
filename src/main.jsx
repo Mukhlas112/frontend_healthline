d39ef3d29fd486b027004466c66c18661646faff
Mukhlas112 <Mukhlaspes@gmail.com>
2025-12-05 23:52:50 +0700

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { NotificationProvider } from './contexts/NotificationContext'; // Import Provider BARU
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    {/* Bungkus Aplikasi dengan Provider Notifikasi */}
+    <NotificationProvider>
+      <App />
+    </NotificationProvider>
   </React.StrictMode>,
 )
